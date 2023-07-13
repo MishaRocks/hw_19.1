0e708b8da947f500801321db034a8dfceb7aebf3
MishaRocks <tehobox@yandex.ru>
2023-07-14 00:30:04 +0300

    Make Post queries

--- main.py
+++ main.py
@@ -8,14 +8,24 @@ serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
 
-    def do_GET(self):
+    def __get_page(self):
         with open('templates/contacts.html', 'r', encoding='UTF-8') as file:
             content = file.read()
+        return content
 
+    def do_GET(self):
+        content = self.__get_page()
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(bytes(content, "utf-8"))
+
+    def do_POST(self):
+        query_components = parse_qs(urlparse(self.path).query)
+        print(query_components)
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
 
 
 if __name__ == "__main__":
