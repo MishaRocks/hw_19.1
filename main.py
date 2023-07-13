30589be3fa99c70a5577bc426dcc62bab721cbd9
MishaRocks <tehobox@yandex.ru>
2023-07-14 00:12:42 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        with open('templates/contacts.html', 'r', encoding='UTF-8') as file:
+            content = file.read()
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(content, "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
